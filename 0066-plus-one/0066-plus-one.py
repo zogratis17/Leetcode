a665731adad024e64dbe1956b0dac4aebcd70cf8
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-02-25 16:57:42 +0530

    Time: 0 ms (100%), Space: 17.6 MB (79.31%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,11 @@
+class Solution:
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        
+        # If all digits were 9, we need to add an additional digit
+        digits = [1] + digits
+        return digits

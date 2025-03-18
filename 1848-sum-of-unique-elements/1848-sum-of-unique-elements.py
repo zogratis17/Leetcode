7ab48dd3f9b487f489073eef20bc71f0197682b9
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-03-18 17:39:33 +0530

    Time: 0 ms (100%), Space: 17.9 MB (34.91%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.py
@@ -1,20 +1,10 @@
-from typing import List
-
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        seen = {}
-        sum_unique = 0
+        count = Counter(nums)
+        sum = 0
+
+        for key , val in count.items():
+            if val == 1:
+                sum += key
         
-        # Count the occurrences of each number
-        for num in nums:
-            if num in seen:
-                seen[num] += 1
-            else:
-                seen[num] = 1
-        
-        # Sum the numbers that appear exactly once
-        for num, count in seen.items():
-            if count == 1:
-                sum_unique += num
-        
-        return sum_unique
+        return sum

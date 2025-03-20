3694d1603659cb4e638f5c773f413c602430df11
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-03-20 17:16:52 +0530

    Time: 0 ms (100%), Space: 18.4 MB (89.99%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,20 +1,18 @@
-class Solution(object):
-    def thirdMax(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums = list(set(nums))
-        
-        first_max = max(nums)
-        nums.remove(first_max)
-        try:
-            second_max = max(nums)
-            nums.remove(second_max)
-            
-            third_max = max(nums)
-        except ValueError:
-            return first_max
-        return third_max
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        m1,m2,m3 = -inf,-inf,-inf
 
-        
+        for i in nums:
+            if i > m1:
+                m1,m2,m3 = i,m1,m2
+
+            if i < m1 and i > m2:
+                m2,m3 = i,m2
+
+            if i < m2 and i > m3:
+                m3 = i
+
+        if m1 != m2 and m2 != m3 and m3 != m1 and m1 > -inf and m2 > -inf and m3 > -inf:
+            return m3
+        else:
+            return m1

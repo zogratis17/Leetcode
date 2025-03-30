9aef658c64701378cea876f3bfb4a2b8631c6e35
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-03-30 22:17:45 +0530

    Time: 4 ms (64.75%), Space: 17.7 MB (90.36%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        summ = 0 
+        n = len(s)
+        i = 0
+
+        d = {'I':1, 'V':5, 'X':10, 'L':50, 'C': 100, 'D': 500, 'M': 1000}
+
+        while i < n:
+            if i < n -1 and d[s[i]] < d[s[i+1]]:
+                summ+= d[s[i+1]] - d[s[i]]
+                i+=2
+            else:
+                summ+= d[s[i]]
+                i+=1
+        return summ

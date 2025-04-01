c9141e349b8a6481400014efa11efbabcde7f22e
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-04-01 09:36:06 +0530

    Time: 15 ms (62.14%), Space: 17.8 MB (62.49%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,15 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        up = down = right = left = 0
+
+        for char in moves:
+            if char=='U':
+                up+=1
+            elif char=='R':
+                right+=1
+            elif char=='L':
+                left+=1
+            elif char=='D':
+                down+=1
+        
+        return up == down and right == left 

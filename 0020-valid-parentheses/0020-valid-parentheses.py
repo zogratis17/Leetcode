383fef3b2388a38449149fc8c9b9a81e37e4514e
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-06-20 18:43:20 +0530

    Time: 3 ms (22.19%), Space: 17.8 MB (51.05%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        brak = {')':'(', '}':'{', ']':'['}
+        stack = []
+
+        for i in s:
+            if i in brak.values():
+                stack.append(i)
+            elif i in brak:
+                if not stack or stack.pop() != brak[i]:
+                    return False
+        return len(stack) == 0
+                

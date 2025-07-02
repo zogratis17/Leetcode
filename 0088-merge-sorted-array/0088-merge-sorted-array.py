452aff7ce5131d5f0d2388dd21ad9c3f76563a84
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-07-02 10:34:51 +0530

    Time: 0 ms (100%), Space: 17.7 MB (71.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        #Slicing Approach
 
         nums1[m:] = nums2
         nums1.sort()
+        

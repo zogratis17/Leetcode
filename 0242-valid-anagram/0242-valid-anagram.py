ef487a216a2c6ce8fafb83b9e8006c5dc44355ef
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-07-09 09:11:08 +0530

    Time: 7 ms (89.43%), Space: 17.8 MB (86.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) ==  Counter(t)
-        # Time : O(N)
-        # Space : O(k) k-> no of distinct char
+        return Counter(s) == Counter(t)

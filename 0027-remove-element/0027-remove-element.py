8152edfb46d49cd01a51bf6b87fb13c5ec35c501
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-08-09 00:13:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,8 +1,3 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0  # track valid elements
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]  
-                k += 1
-        return k
+        

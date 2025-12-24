ef826075d58f167a71632632d877222fb077e0e0
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-12-24 15:50:56 +0530

    Time: 6 ms (46.55%), Space: 18.7 MB (90.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nz , i = 0,0
+        nz , i = 0, 0
         while i < len(nums):
             if nums[i] != 0:
-                nums[nz],nums[i] = nums[i],nums[nz]
+                nums[nz] , nums[i] = nums [i] , nums[nz]
                 nz+=1
+            
             i+=1
+
+        
